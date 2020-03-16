6721c68d2f1cffff8b50795136248aa61673300d
SophonAlpha <SophonAlpha@users.noreply.github.com>
2020-03-16 22:59:54 +0400

    Initial commit

---
+++ hsbccc2csv.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+HSBC credit card statement PDF to CSV converter.
+"""
+
+
+import PyPDF2
+import pdfplumber
+
+
+if __name__ == '__main__':
+    pass
