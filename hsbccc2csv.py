c2aca30d74b1b9692ddc21d8331f0c208d20468e
SophonAlpha <SophonAlpha@users.noreply.github.com>
2020-03-18 09:01:07 +0400

    add transformation of transaction information

--- hsbccc2csv.py
+++ hsbccc2csv.py
@@ -8,6 +8,7 @@ HSBC credit card statement PDF to CSV converter.
 import argparse
 import glob
 import re
+import math
 import pdfplumber
 
 
@@ -17,10 +18,20 @@ def main():
     for _, pdf_file in enumerate(pdf_files):
         text = extract_text(pdf_file)
         stmt_date = get_stmt_date(text)
+        debit_value = get_debit_value(text)
         transactions = extract_transaction_lines(text)
         transactions = strip_spaces(transactions)
         transactions = string2float(transactions)
         transactions = change_date_fmt(transactions, stmt_date)
+        total_amount = get_total_amount(transactions)
+        if math.isclose(debit_value, total_amount, abs_tol=0.001):
+            pass
+        else:
+            print('Error! Total amount of new transactions does not '
+                  'match to be debited amount.')
+            print(f'amount new transactions = {total_amount}')
+            print(f'amount to be debited = {debit_value}')
+            break
 
 
 def get_stmt_date(text):
@@ -29,6 +40,14 @@ def get_stmt_date(text):
         r'(?<=Statement Date  )[0-9]{2}' + months + r'[0-9]{4}',
         re.MULTILINE)
     return pattern.search(text)[0]
+
+
+def get_debit_value(text):
+    pattern = re.compile(
+        r'(?<=Your specified account will be debited for AED )[0-9]*,?[0-9]+\.[0-9]{2}',
+        re.MULTILINE)
+    debit_value = -1 * float(pattern.search(text)[0].replace(',', ''))
+    return debit_value
 
 
 def extract_text(pdf_file):
@@ -67,14 +86,34 @@ def string2float(transactions):
     return txns_updated
 
 
-def change_date_fmt(transactions):
-    txns_updated = []
-    for txn in transactions:
-        # txn['PostingDate'] =
-        # txn['TransactionDate'] =
+def change_date_fmt(transactions, stmt_date):
+    mmap= {'JAN': '01', 'FEB': '02', 'MAR': '03', 'APR': '04',
+           'MAY': '05', 'JUN': '06', 'JUL': '07', 'AUG': '08',
+           'SEP': '09', 'OCT': '10', 'NOV': '11', 'DEC': '12'}
+    stmt_month = stmt_date[-7:-4]
+    stmt_year = int(stmt_date[-4:])
+    for idx, txn in enumerate(transactions):
+        post_day = txn['PostingDate'][:2]
+        post_month= txn['PostingDate'][-3:]
+        if stmt_month == 'JAN' and post_month == 'DEC':
+            txn['PostingDate'] = f'{post_day}/{mmap[post_month]}/{stmt_year - 1}'
+        else:
+            txn['PostingDate'] = f'{post_day}/{mmap[post_month]}/{stmt_year}'
+        txn_day = txn['TransactionDate'][:2]
+        txn_month= txn['TransactionDate'][-3:]
+        if stmt_month == 'JAN' and txn_month == 'DEC':
+            txn['TransactionDate'] = f'{txn_day}/{mmap[txn_month]}/{stmt_year - 1}'
+        else:
+            txn['TransactionDate'] = f'{txn_day}/{mmap[txn_month]}/{stmt_year}'
+        transactions[idx] = txn
+    return transactions
 
-        txns_updated.append(txn)
-    return txns_updated
+
+def get_total_amount(transactions):
+    amount = sum([txn['Amount'] for txn in transactions])
+    paid = sum([txn['Amount'] for txn in transactions
+                if txn['TransactionDetails'].find('PAY BY') >= 0])
+    return amount - paid
 
 
 def parse_args():
